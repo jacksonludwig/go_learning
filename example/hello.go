1afdcb982ec00c512e14f78a50091e4142dbd5cc
Jackson Ludwig <jacksonludwig0@gmail.com>
2020-12-29 19:11:20 -0500

    some more examples

--- example/hello.go
+++ example/hello.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "math"
+)
 
 const s string = "constant"
 
@@ -69,4 +72,46 @@ func zeroptr(iptr *int) {
     *iptr = 0
 }
 
+
+type person struct {
+    name string
+    age int
+}
+func newPerson(name string) *person {
+    p := person{name: name}
+    p.age = 42
+    return &p
+}
+
+type geometry interface {
+    area() float64
+    perim() float64
+}
+
+type rect struct {
+    width, height float64
+}
+type circle struct {
+    radius float64
+}
+
+func (r rect) area() float64 {
+    return r.width * r.height
+}
+func (r rect) perim() float64 {
+    return 2*r.width + 2*r.height
+}
+
+func (c circle) area() float64 {
+    return math.Pi * c.radius * c.radius
+}
+func (c circle) perim() float64 {
+    return 2 * math.Pi * c.radius
+}
+
+func measure(g geometry) {
+    fmt.Println(g)
+    fmt.Println(g.area())
+    fmt.Println(g.perim())
+}
 
